20af5adde7d675c5499ce2924a47e1e95e0e9423
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 03:43:38 -0400

    Fix handler func signature in router

--- src/server/router.go
+++ src/server/router.go
@@ -2,26 +2,26 @@ package server
 
 import (
     "net/http"
-    "log"
     "regexp"
 )
 
 var validPath = regexp.MustCompile("^/(edit|save|view|login|loginresult|signup)/")
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, *Server), srv *Server) http.HandlerFunc {
+func validateURL(
+        handlerFunc func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
 			http.NotFound(w, r)
 			return
 		}
-		fn(w, r, srv)
+		handlerFunc(w, r)
 	}
 }
 
 func Route(srv *Server) {
-	http.HandleFunc("/login/", makeHandler(LoginHandler, srv))
-	http.HandleFunc("/loginresult/", makeHandler(LoginresultHandler, srv))
-	http.HandleFunc("/signup/", makeHandler(SignupHandler, srv))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    var factory HandlerFuncFactory
+	http.HandleFunc("/login/", validateURL(factory.CreateByServerHandlerFunc(LoginHandler, srv)))
+	http.HandleFunc("/loginresult/", validateURL(factory.CreateByServerHandlerFunc(LoginresultHandler, srv)))
+	http.HandleFunc("/signup/", validateURL(factory.CreateByServerHandlerFunc(SignupHandler, srv)))
 }
