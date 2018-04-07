1c3a8f3284328f3cb32887dc25e35cce504b19d1
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 01:01:41 -0400

    Quick fix path problem in render

--- src/libs/render/render.go
+++ src/libs/render/render.go
@@ -5,10 +5,10 @@ import (
     "net/http"
 )
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html", "login.html", "loginresult.html", "signup.html"))
+var templates = template.Must(template.ParseFiles("pages/login.html", "pages/loginresult.html", "pages/signup.html"))
 
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	err := templates.ExecuteTemplate(w, "../pages/"+tmpl+".html", data)
+	err := templates.ExecuteTemplate(w, tmpl+".html", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
