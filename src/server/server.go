b4c79e5b275d12cf20f2d94a85b2bc99f49fa5fc
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 17:36:09 -0400

    Validate and Register User Server

--- src/server/server.go
+++ src/server/server.go
@@ -4,6 +4,8 @@ import (
     "html/template"
     "net/http"
     "log"
+    "regexp"
+    "errors"
 )
 
 type Server struct {
@@ -12,12 +14,53 @@ type Server struct {
     handlers map[string]http.HandlerFunc // api -> handler
     templates *template.Template
 
+    validUserName *regexp.Regexp
+    validPassword *regexp.Regexp
 }
 
 func (srv *Server) Init() {
     srv.users = make(map[string]*User)
     srv.htmls = make(map[string]string)
     srv.handlers = make(map[string]http.HandlerFunc)
+
+    srv.validUserName, _ = regexp.Compile("^[a-zA-Z0-9]{4,10}$")
+    srv.validPassword, _ = regexp.Compile("^[a-zA-Z0-9]{4,10}$")
+}
+
+func (srv *Server) validateUserNameAndPassFormat(username string, password string) (bool, error) {
+    if !srv.validUserName.Match([]byte(username)) || !srv.validPassword.Match([]byte(password)) {
+        return false, errors.New("Incorrect username or password format")
+    }
+    return true, nil
+}
+
+func (srv *Server) ValidateUser(username string, password string) (bool, error) {
+    if ok, err := srv.validateUserNameAndPassFormat(username, password); !ok {
+        return ok, err
+    }
+    if user, ok := srv.users[username]; ok {
+        if user.Password != password {
+            return false, errors.New("Incorrect password")
+        }
+        return true, nil
+    }
+    return false, errors.New("User not exists")
+}
+
+func (srv *Server) RegisterUser(username string, password string) (bool, error) {
+    if ok, err := srv.validateUserNameAndPassFormat(username, password); !ok {
+        return ok, err
+    }
+    if _, ok := srv.users[username]; ok {
+        return false, errors.New("User already exists")
+    }
+    newUser := &User{
+        Username:username,
+        Password:password,
+    }
+    newUser.Init()
+    srv.users[username] = newUser
+    return true, nil
 }
 
 func (srv *Server) RegisterHTML(name string, path string) {
