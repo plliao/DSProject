156a96107fbb8c7b0b613cd8de89938326f7b125
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 05:19:43 -0400

    Implement registerHandlerFunc in server

--- src/server/server.go
+++ src/server/server.go
@@ -9,6 +9,7 @@ import (
 type Server struct {
     users map[string]*User
     htmls map[string]string // name -> filepath
+    handlers map[string]http.HandlerFunc // api -> handler
     templates *template.Template
 
 }
@@ -16,11 +17,15 @@ type Server struct {
 func (srv *Server) Init() {
     srv.users = make(map[string]*User)
     srv.htmls = make(map[string]string)
-
+    srv.handlers = make(map[string]http.HandlerFunc)
 }
 
 func (srv *Server) RegisterHTML(name string, path string) {
     srv.htmls[name] = path
+}
+
+func (srv *Server) RegisterHandlerFunc(api string, handler http.HandlerFunc) {
+    srv.handlers[api] = handler
 }
 
 func (srv *Server) Start(port string) {
@@ -35,4 +40,20 @@ func (srv *Server) createTemplates() {
         filepaths = append(filepaths, filepath)
     }
     srv.templates = CreateTemplates(filepaths...)
+}
+
+func (srv *Server) GetAPI() []string {
+    apis := make([]string, 0, len(srv.handlers))
+    for api, _ := range srv.handlers {
+        apis = append(apis, api)
+    }
+    return apis
+}
+
+func (srv *Server) GetHandlers() []http.HandlerFunc {
+    handlers := make([]http.HandlerFunc, 0, len(srv.handlers))
+    for _, handler := range srv.handlers {
+        handlers = append(handlers, handler)
+    }
+    return handlers
 }
