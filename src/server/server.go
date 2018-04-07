5b48ad2f110554b3bcf475cdbb3dea1c1fdbd9ac
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 03:10:05 -0400

    Implement RegisterHTML in server

--- src/server/server.go
+++ src/server/server.go
@@ -1,8 +1,38 @@
 package server
 
 import (
+    "html/template"
+    "net/http"
+    "log"
 )
 
 type Server struct {
-    Users map[string]*User
+    users map[string]*User
+    htmls map[string]string // name -> filepath
+    templates *template.Template
+
+}
+
+func (srv *Server) Init() {
+    srv.users = make(map[string]*User)
+    srv.htmls = make(map[string]string)
+
+}
+
+func (srv *Server) RegisterHTML(name string, path string) {
+    srv.htmls[name] = path
+}
+
+func (srv *Server) Start(port string) {
+    srv.createTemplates()
+    Route(srv)
+    log.Fatal(http.ListenAndServe(":" + port, nil))
+}
+
+func (srv *Server) createTemplates() {
+    filepaths := make([]string, 0, len(srv.htmls))
+    for _, filepath := range srv.htmls {
+        filepaths = append(filepaths, filepath)
+    }
+    srv.templates = CreateTemplates(filepaths...)
 }
