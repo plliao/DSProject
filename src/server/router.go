f2ccabfb59e7363cb22ff8b90305e94d0ce2116c
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 05:19:03 -0400

    Use decorator to manage middleware

--- src/server/router.go
+++ src/server/router.go
@@ -3,14 +3,37 @@ package server
 import (
     "net/http"
     "regexp"
+    "strings"
 )
 
-var validPath = regexp.MustCompile("^/(edit|save|view|login|loginresult|signup)/")
+type Middleware interface {
+    Decorate(http.HandlerFunc) http.HandlerFunc
+}
 
-func validateURL(
-        handlerFunc func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+type MiddlewareManager struct {
+    decorators []*Middleware
+}
+
+func (manager *MiddlewareManager) Decorate(handlerFunc http.HandlerFunc) http.HandlerFunc {
+    decoratedHandlerFunc := handlerFunc
+    for _, decorator := range manager.decorators {
+        decoratedHandlerFunc = (*decorator).Decorate(decoratedHandlerFunc)
+    }
+    return decoratedHandlerFunc
+}
+
+func (manager *MiddlewareManager) RegisterMiddleware(middleware Middleware) {
+    manager.decorators = append(manager.decorators, &middleware)
+}
+
+
+type URLValidator struct {
+    validPath *regexp.Regexp
+}
+
+func (validator URLValidator) Decorate(handlerFunc http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
+		m := validator.validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
 			http.NotFound(w, r)
 			return
@@ -20,8 +43,17 @@ func validateURL(
 }
 
 func Route(srv *Server) {
-    var factory HandlerFuncFactory
-	http.HandleFunc("/login/", validateURL(factory.CreateByServerHandlerFunc(LoginHandler, srv)))
-	http.HandleFunc("/loginresult/", validateURL(factory.CreateByServerHandlerFunc(LoginresultHandler, srv)))
-	http.HandleFunc("/signup/", validateURL(factory.CreateByServerHandlerFunc(SignupHandler, srv)))
+    var manager MiddlewareManager
+
+    apis := srv.GetAPI()
+    validator := URLValidator{
+        validPath:regexp.MustCompile("^/" + strings.Join(apis, "|") + "/"),
+    }
+    manager.RegisterMiddleware(validator)
+
+    handlers := srv.GetHandlers()
+    for index, handler := range handlers {
+        decoratedHandler := manager.Decorate(handler)
+        http.HandleFunc("/" + apis[index] + "/", decoratedHandler)
+    }
 }
