d6a90b24a888a3c9c0f9e012bc6a646e3ce93afc
Pei-Lun Liao <pll273@nyu.edu>
2018-04-07 03:09:25 -0400

    Fix render function signature in handler

--- src/server/handler.go
+++ src/server/handler.go
@@ -14,14 +14,14 @@ type LogResult struct {
 func LoginHandler(w http.ResponseWriter, r *http.Request, srv *Server) {
 	//http.Redirect(w, r, "/logresult", http.StatusFound)
 	var u User
-	RenderTemplate(w, "login", u)
+	RenderTemplate(w, srv, "login", u)
 	u.Username = r.FormValue("name")
 	u.Password = r.FormValue("password")
 }
 func LoginresultHandler(w http.ResponseWriter, r *http.Request, srv *Server) {
 	logres := LogResult { Name:r.FormValue("name"), Password:r.FormValue("password")}
 	if(r.FormValue("choose") == "Log in"){
-		pw, ok := srv.Users[r.FormValue("name")]
+		pw, ok := srv.users[r.FormValue("name")]
 		if(pw.Password == r.FormValue("password")){
 			logres.Result = "successfully"
 			//
@@ -32,17 +32,17 @@ func LoginresultHandler(w http.ResponseWriter, r *http.Request, srv *Server) {
 				logres.Message = "Wrong password."
 			}
 		}
-		RenderTemplate(w, "loginresult", logres)
+		RenderTemplate(w, srv, "loginresult", logres)
 
 	}else{
 		tmp := User{Username: r.FormValue("name"), Password:r.FormValue("password")}
-		srv.Users[r.FormValue("name")] = &tmp
-		RenderTemplate(w, "signup", logres)
+		srv.users[r.FormValue("name")] = &tmp
+		RenderTemplate(w, srv, "signup", logres)
 		//http.Redirect(w, r, "/signup/", http.StatusFound)
 	}
 }
 
 func SignupHandler(w http.ResponseWriter, r *http.Request, srv *Server) {
 	new_user := User{ Username:r.FormValue("name"), Password:r.FormValue("password")}
-	RenderTemplate(w, "signup", new_user)
+	RenderTemplate(w, srv, "signup", new_user)
 }
