97d709dea3aae1761f13e311cb68ce5c56ecc060
Pei-Lun Liao <pll273@nyu.edu>
2018-04-20 14:54:45 -0400

    Seperate Front end and Back end server code

--- src/backEnd/server.go
+++ src/backEnd/server.go
@@ -12,24 +12,12 @@ import (
 
 type Server struct {
     users map[string]*User
-    htmls map[string]string // name -> filepath
-    handlers map[string]http.HandlerFunc // api -> handler
-    templates *template.Template
-
-    validUserName *regexp.Regexp
-    validPassword *regexp.Regexp
-
     tokens map[string]*User
 }
 
 func (srv *Server) Init() {
     srv.users = make(map[string]*User)
-    srv.htmls = make(map[string]string)
     srv.tokens = make(map[string]*User)
-    srv.handlers = make(map[string]http.HandlerFunc)
-
-    srv.validUserName, _ = regexp.Compile("^[a-zA-Z0-9]{4,10}$")
-    srv.validPassword, _ = regexp.Compile("^[a-zA-Z0-9]{4,10}$")
 }
 
 func (srv *Server) validateUserNameAndPassFormat(username string, password string) (bool, error) {
@@ -98,40 +86,6 @@ func (srv *Server) DeleteUser(user *User) {
     delete(srv.users, user.Username)
 }
 
-func (srv *Server) RegisterHTML(name string, path string) {
-    srv.htmls[name] = path
-}
-
-func (srv *Server) RegisterHandlerFunc(api string, handler http.HandlerFunc) {
-    srv.handlers[api] = handler
-}
-
 func (srv *Server) Start(port string) {
-    srv.createTemplates()
-    Route(srv)
-    log.Fatal(http.ListenAndServe(":" + port, nil))
-}
 
-func (srv *Server) createTemplates() {
-    filepaths := make([]string, 0, len(srv.htmls))
-    for _, filepath := range srv.htmls {
-        filepaths = append(filepaths, filepath)
-    }
-    srv.templates = CreateTemplates(filepaths...)
-}
-
-func (srv *Server) GetAPI() []string {
-    apis := make([]string, 0, len(srv.handlers))
-    for api, _ := range srv.handlers {
-        apis = append(apis, api)
-    }
-    return apis
-}
-
-func (srv *Server) GetHandlers() []http.HandlerFunc {
-    handlers := make([]http.HandlerFunc, 0, len(srv.handlers))
-    for _, handler := range srv.handlers {
-        handlers = append(handlers, handler)
-    }
-    return handlers
 }
