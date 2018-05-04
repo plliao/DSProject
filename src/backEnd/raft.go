a5e6ec9558684b88fc32a4a34751bbd12e486596
Pei-Lun Liao <pll273@nyu.edu>
2018-05-03 22:35:55 -0400

    Fix Raft conflict

--- src/backEnd/raft.go
+++ src/backEnd/raft.go
@@ -42,7 +42,7 @@ type RequestVoteReply struct {
     VoteGranted bool
 }
 
-func (raft Raft) AppendEntry(args AppendEntryArgs, reply *AppendEntryReply) error {
+func (raft *Raft) AppendEntry(args AppendEntryArgs, reply *AppendEntryReply) error {
     fmt.Print("Receive AppendEntry\n")
     if args.Term < raft.term || raft.logTerms[args.PrevLogIndex] != args.PrevLogTerm{
         reply.Success = false
@@ -58,24 +58,27 @@ func (raft Raft) AppendEntry(args AppendEntryArgs, reply *AppendEntryReply) erro
         for i:= raft.commitIndex; i<= args.CommitIndex; i++{
             //exec
         }
-        raft.commitIndex = min(args.CommitIndex, len(logs)-1)
+        if args.CommitIndex < len(raft.logs) - 1 {
+            raft.commitIndex = args.CommitIndex
+        } else {
+            raft.commitIndex = len(raft.logs) - 1
+        }
     }
 
-    reply.term = raft.term
+    reply.Term = raft.term
     reply.Success = true
     return nil
 }
 
-func (raft Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
-    if args.Term < raft.Term {
+func (raft *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error {
+    if args.Term < raft.term {
         reply.VoteGranted = false
-    } else if ((raft.voteFor < 0 || raft.voteFor == args.CandidateId)
-        && len(raft.logs)-1 <= args.lastLogIndex
-        && raft.logTerms[len(raft.logTerms)-1] <= args.LastLogTerm){
+    } else if ((raft.voteFor < 0 || raft.voteFor == args.CandidateId) &&
+            len(raft.logs)-1 <= args.LastLogIndex &&
+            raft.logTerms[len(raft.logTerms)-1] <= args.LastLogTerm) {
         reply.VoteGranted = true
         reply.Term = raft.term
         raft.voteFor = args.CandidateId
     }
-    reply.VoteGranted = false
     return nil
 }
